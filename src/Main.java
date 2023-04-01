01862b4400f4ff69cb63a686f5a3faa3451c0ce0
Tkakishak <tkakishakd@mail.ru>
2023-04-01 20:45:54 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
-
-
+        String myWord = "топот";
+        StringBuffer buffer = new StringBuffer(myWord);
+        buffer.reverse();
+        String data = buffer.toString();
+        if (myWord.equals(data)){
+            System.out.println("Слово является палиндромом");
+        }else {
+            System.out.println("Слово НЕ является палиндромом");
+        }
     }
 }
